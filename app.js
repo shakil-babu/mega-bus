ee9420dcc66191ed5087ed4fd99a1d95624eddf3
Shakil-Babu <personal.shakil.babu@gmail.com>
2021-01-29 02:24:16 -0800

    updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ firstClassIncrement.addEventListener('click' , function(){
 	ticketPriceHandler(true , 'first-class');
 	
 });
-var economyIncrement = document.getElementById('economy-increment') ;
+const economyIncrement = document.getElementById('economy-increment') ;
 economyIncrement.addEventListener('click', function(){
     ticketPriceHandler(true, 'economy');
 })
@@ -18,7 +18,7 @@ const firstClassDecrement  = document.getElementById('first-class-decrement');
 firstClassDecrement.addEventListener('click' , function () {
 	ticketPriceHandler(false , 'first-class');
 }) 
-var economyDecrement = document.getElementById('economy-decrement') ;
+const economyDecrement = document.getElementById('economy-decrement') ;
 economyDecrement.addEventListener('click', function(){
 	ticketPriceHandler(false , 'economy');
 })
@@ -42,8 +42,56 @@ function ticketPriceHandler(isIncrement, ticketName) {
 
 	if(ticketName == 'economy'){
 		ticketPrice = ticketCount * 100;
-    }
+	}
+	
+	Calculation(); 
 }
+
+// calculation function
+function Calculation(){
+	const firstClassInput = document.getElementById('first-class-input');
+	const firstClassValue = parseInt(firstClassInput.value);
+
+	const economyInput = document.getElementById('economy-input');
+	const economyValue = parseInt(economyInput.value);
+
+
+	// for sub total
+	let subTotalPrice = firstClassValue * 150 + economyValue * 100 ;
+	document.getElementById('sub-total').innerText = '$'+subTotalPrice ;
+
+	// for vat
+	let ticketVat = Math.floor(subTotalPrice * 10/100);
+	document.getElementById('ticket-vat').innerText = '$'+ticketVat ;
+
+	// for totalPrice 
+	let totalPrice = subTotalPrice + ticketVat ;
+	document.getElementById('total-price').innerText = '$'+totalPrice; 
+}
+
+
+// for book
+let bookNowBtn = document.getElementById('book-now').addEventListener('click', function() {
+	const firstClassInput = document.getElementById('first-class-input');
+	const economyInput = document.getElementById('economy-input');
+
+	// validation and show completed ui.
+	if(firstClassInput.value == 0 && economyInput.value == 0){
+		alert('Take your ticket!')
+	}else{
+		document.getElementById('book-completed').style.visibility='visible';
+		setTimeout(() => {
+			document.getElementById('book-completed').style.visibility='hidden';
+		}, 2000);
+
+		firstClassInput.value = 0 ;
+		economyInput.value = 0 ;
+		document.getElementById('sub-total').innerText = '$'+0;
+		document.getElementById('ticket-vat').innerText = '$'+0 ;
+		document.getElementById('total-price').innerText = '$'+0; 
+	}
+})
+
 
 
 
